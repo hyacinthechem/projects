19c7ffc8902a1698ae8bb125a3b33e8da947a121
hyacinthechem <chemaslehyacinthe@gmail.com>
2024-09-13 00:05:04 +1200

    Changes to loading

--- Week 8 - COMP103/STWOfficials.java
+++ Week 8 - COMP103/STWOfficials.java
@@ -1,7 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import ecs100.*;
 
 public class STWOfficials {
+
+    private TitleRank theTree;
+
 
     public void setupGUI(){
         UI.addButton("Print STW Hierachy", this::printOfficials);
@@ -11,11 +17,57 @@ public class STWOfficials {
 
     public void printOfficials(){
 
+        iterativePrinting();
 
     }
 
+    public void iterativePrinting(){
+
+        TitleRank node = new TitleRank(theTree.getName(), theTree.getPositions());
+
+        while(!node.finalRank()){
+            TitleRank current = node;
+            current = current.getLeftSubRank();
+            UI.println(current.toString());
+            current = current.getRightSubRank();
+            UI.println(current.toString());
+
+
+        }
+
+    }
+
+    public void loadTree (String filename) {
+        if (!Files.exists(Path.of(filename))){
+            UI.println("No such file: "+filename);
+            return;
+        }
+        try{theTree = loadSubTree(new ArrayDeque<String>(Files.readAllLines(Path.of(filename))));}
+        catch(IOException e){UI.println("File reading failed: " + e);}
+    }
+
+
+    public TitleRank loadSubTree(Queue<String> lines){
+        Scanner line = new Scanner(lines.poll());
+        String type = line.next();
+        String title = line.next();
+        String text = line.nextLine().trim();
+        TitleRank node = new TitleRank(title,text);
+        //if (type.contains("President:") || type.contains("Governor-General:") || type.contains("Heads-Of-Departments:" )){
+        if(type.equals("Question:")){
+        TitleRank yesCh = loadSubTree(lines);
+            TitleRank noCh = loadSubTree(lines);
+            node.setRanks(yesCh, noCh);
+        }
+        return node;
+
+    }
+
+
+
     public static void main(String[] args){
         STWOfficials stw = new STWOfficials();
+        stw.loadTree("Hierachy.txt");
         stw.setupGUI();
 
 
