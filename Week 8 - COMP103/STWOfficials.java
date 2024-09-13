45276f847418bb1397a03aecffdb14ad8392d3e4
hyacinthechem <chemaslehyacinthe@gmail.com>
2024-09-13 22:13:43 +1200

    Pre-order traversal for both iteration and recursion

--- Week 8 - COMP103/STWOfficials.java
+++ Week 8 - COMP103/STWOfficials.java
@@ -11,6 +11,7 @@ public class STWOfficials {
 
     public void setupGUI(){
         UI.addButton("Print STW Hierachy", this::printOfficials);
+        UI.addButton("Draw STW Tree", this::drawOfficials);
 
 
     }
@@ -19,21 +20,93 @@ public class STWOfficials {
 
         iterativePrinting();
 
+        for(int i = 0; i<10; i++){
+            UI.println("-------------------");
+            UI.println("-------------------");
+        }
+
+        iterativePrintingPreOrder();
+
+        for(int i = 0; i<10; i++){
+            UI.println("-------------------");
+            UI.println("-------------------");
+        }
+
+        recursivePreOrder(theTree);
+
     }
 
     public void iterativePrinting(){
 
-        TitleRank node = new TitleRank(theTree.getName(), theTree.getPositions());
+       // TitleRank node = new TitleRank(theTree.getName(), theTree.getPositions());
+        TitleRank node = theTree;
+        UI.println(node.toString());
 
-        while(!node.finalRank()){
-            TitleRank current = node;
-            current = current.getLeftSubRank();
-            UI.println(current.toString());
-            current = current.getRightSubRank();
-            UI.println(current.toString());
+        while(!node.finalRank()){  //recurse right side of the tree
+
+            node = node.getLeftSubRank();
+            UI.println(node.toString());
+            node = node.getRightSubRank();
+            UI.println(node.toString());
 
 
         }
+
+        node = theTree;
+
+        UI.println(node.toString());
+
+        while(!node.finalRank()){ //recurse left side of the tree
+            node = node.getRightSubRank();
+            UI.println(node.toString());
+            node = node.getLeftSubRank();
+            UI.println(node.toString());
+
+        }
+
+    }
+
+    public void iterativePrintingPreOrder(){
+
+        if(theTree==null){
+            return;
+        }
+
+
+        TitleRank node = theTree;
+
+        Stack<TitleRank> stack = new Stack<>();
+
+        //root, left, right
+        //process root push it then push all
+        stack.push(node);
+        while(!stack.isEmpty()){
+            TitleRank temp = stack.pop();
+            UI.println(temp.toString());
+            if(!(temp.getRightSubRank() == null)){
+                stack.push(temp.getRightSubRank());
+            }
+
+            if(!(temp.getLeftSubRank() == null)){
+                stack.push(temp.getLeftSubRank());
+            }
+        }
+
+
+    }
+
+    public void recursivePreOrder(TitleRank node){
+
+
+        if(node==null){   //base case: stop recursion when no more items to traverse in the tree
+            return;
+        }
+
+        UI.println(node.toString());
+        recursivePreOrder(node.getLeftSubRank());
+        recursivePreOrder(node.getRightSubRank());
+
+
 
     }
 
@@ -52,7 +125,7 @@ public class STWOfficials {
         String type = line.next();
         String title = line.next();
         String text = line.nextLine().trim();
-        TitleRank node = new TitleRank(title,text);
+        TitleRank node = new TitleRank(text,title);
         //if (type.contains("President:") || type.contains("Governor-General:") || type.contains("Heads-Of-Departments:" )){
         if(type.equals("Question:")){
         TitleRank yesCh = loadSubTree(lines);
@@ -63,6 +136,24 @@ public class STWOfficials {
 
     }
 
+    public void drawOfficials(){
+
+        recursiveDrawer(theTree, 100, 200);
+
+    }
+
+    public void recursiveDrawer(TitleRank node, double x, double y){
+        if(node==null){
+            return;
+        }
+
+        node.draw(x,y);
+
+
+        recursiveDrawer(node.getLeftSubRank(),x+100,y-50);
+        recursiveDrawer(node.getRightSubRank(),x+100,y+50);
+
+    }
 
 
     public static void main(String[] args){
